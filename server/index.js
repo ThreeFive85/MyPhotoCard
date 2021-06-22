0dd23978661e64bb6f23da41d31212a81405aa57
박동혁 <kaje0god@naver.com>
2021-06-22 19:43:17 +0900

    Feat : server index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mysql from 'mysql2';
+import cors from 'cors';
+
+import usersRoutes from './routes/users.js';
+
+import { db } from './config/db.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/users', usersRoutes);
+
+
+const PORT = process.env.PORT || 4000;
+
+const connection = mysql.createConnection(db);
+
+connection.connect();
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
