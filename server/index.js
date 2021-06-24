7bc36be8905fab0adfd27312d4c84694c75d5b94
박동혁 <kaje0god@naver.com>
2021-06-24 22:11:22 +0900

    Feat : cardsRoutes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mysql from 'mysql2';
 import cors from 'cors';
 
 import usersRoutes from './routes/users.js';
+import cardsRoutes from './routes/cards.js';
 
 import { db } from './config/db.js';
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/users', usersRoutes);
+app.use('/cards', cardsRoutes);
 
 
 const PORT = process.env.PORT || 4000;
