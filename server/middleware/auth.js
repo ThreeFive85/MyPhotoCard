b82315c0dcda5f08e3a0881e87c5b593ece3f621
박동혁 <kaje0god@naver.com>
2021-06-25 19:31:50 +0900

    Feat : middleware folder

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import { tokenPassword } from '../config/token.js';
+
+const auth = async (req, res, next) => {
+  try {
+    // console.log(req.headers.authorization)
+    const token = req.headers.authorization;
+    // console.log(token)
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {      
+      decodedData = jwt.verify(token, tokenPassword);
+      // console.log('decodedData', decodedData);
+      req.userEmail = decodedData?.email;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userEmail = decodedData?.sub;
+    }    
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
