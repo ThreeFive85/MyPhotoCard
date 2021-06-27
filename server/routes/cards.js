2fa3f94fbe180a84f06fd372a3dfeba7ff6f742d
박동혁 <kaje0god@naver.com>
2021-06-27 18:12:12 +0900

    Feat : createCard router

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getCards } from '../controllers/cards.js';
+import { getCards, createCard } from '../controllers/cards.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', auth, getCards);
+router.post('/', auth, createCard);
 
 export default router;
