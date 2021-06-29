e7474a0131236e6e09cf5e9faff15c276f0a6ea9
박동혁 <kaje0god@naver.com>
2021-06-29 20:15:00 +0900

    Feat : updateCard, deleteCard function router

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { getCards, createCard } from '../controllers/cards.js';
+import { getCards, createCard, updateCard, deleteCard } from '../controllers/cards.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', auth, getCards);
 router.post('/', auth, createCard);
+router.patch('/:id', auth, updateCard);
+router.delete('/:id', auth, deleteCard);
 
 export default router;
