b1661f814cc230b8a1c07e84108d99ab69225c56
박동혁 <kaje0god@naver.com>
2021-07-01 00:23:25 +0900

    Feat : Navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
+
+import useStyles from './styles';
+import photoCard from '../../images/photoIcon.png';
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    // const dispatch = useDispatch();
+    // const history = useHistory();
+    // const location = useLocation();
+    
+    // console.log(user)
+
+    // useEffect(() => {
+    //     const token = user?.token;
+
+    //     if(token) {
+    //         const decodedToken = decode(token);
+
+    //         if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+    //     }
+
+    //     setUser(JSON.parse(localStorage.getItem('profile')));
+
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [location]);
+
+    const logout = () => {
+        // dispatch({ type: 'LOGOUT'});
+
+        // history.push('/');
+
+        // setUser(null);
+    }
+
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to='/' className={classes.heading} variant="h2" align="center">나만의 포토 카드</Typography>
+                <img className={classes.image} src={photoCard} alt="photoCard" height="60" />
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">로그인</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar
