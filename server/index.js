006e116a03c5a2dc8f6807e5a37ead14c37e1424
박동혁 <kaje0god@naver.com>
2021-07-06 04:20:44 +0900

    Fix : user route

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
-app.use('/users', usersRoutes);
+app.use('/user', usersRoutes);
 app.use('/cards', cardsRoutes);
 
 
