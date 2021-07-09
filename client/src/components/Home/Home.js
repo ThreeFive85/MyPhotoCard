a38356459404c64cb822a580e88bab98021706f0
박동혁 <kaje0god@naver.com>
2021-07-10 03:24:28 +0900

    Feat: Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+
+import { getCards } from '../../actions/posts';
+import useStyles from '../../styles';
 
 function Home() {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCards());
+    }, [dispatch]);
+
     return (
-        <div>
-            초기 화면 페이지
-        </div>
+        <Grow in>
+            <Container>
+                <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId}/>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
     )
 }
 
-export default Home;
+export default Home
