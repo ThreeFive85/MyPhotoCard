d14262edc6b28b6f364d38be512b1627e3e91722
박동혁 <kaje0god@naver.com>
2021-07-16 06:57:29 +0900

    Feat: fetchUserCards, lockCard

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,8 +11,10 @@ API.interceptors.request.use((req) => {
 });
 
 export const fetchCards = () => API.get('/cards');
+export const fetchUserCards = () => API.get('/cards/user');
 export const createCard = (newPost) => API.post('/cards', newPost);
 export const updateCard = (id, updatedPost) => API.patch(`/cards/${id}`, updatedPost);
+export const lockCard = (id) => API.patch(`/cards/${id}/locked`);
 export const deleteCard = (id) => API.delete(`/cards/${id}`);
 
 export const signIn = (formData) => API.post('/user/signin', formData);
