737ae00faae3a38cd2f77974ae1604319f328429
박동혁 <kaje0god@naver.com>
2021-07-21 01:58:28 +0900

    Fix : add upload funtion in post router

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,11 +3,13 @@ import express from 'express';
 import { getCards, createCard, updateCard, deleteCard, getUserCards, lockCard } from '../controllers/cards.js';
 import auth from '../middleware/auth.js';
 
+import { upload } from '../controllers/upload.js';
+
 const router = express.Router();
 
 router.get('/', getCards);
 router.get('/user', auth, getUserCards);
-router.post('/', auth, createCard);
+router.post('/', auth, upload.single('photo'), createCard);
 router.patch('/:id', auth, updateCard);
 router.patch('/:id/locked', auth, lockCard);
 router.delete('/:id', auth, deleteCard);
