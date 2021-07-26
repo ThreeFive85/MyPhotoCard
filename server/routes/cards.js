f58b616900bcef75e2c1e1f01e22aa952732c734
박동혁 <kaje0god@naver.com>
2021-07-26 14:08:42 +0900

    Feat: deleteImg middleware

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,15 +3,15 @@ import express from 'express';
 import { getCards, createCard, updateCard, deleteCard, getUserCards, lockCard } from '../controllers/cards.js';
 import auth from '../middleware/auth.js';
 
-import { upload } from '../controllers/upload.js';
+import { upload, deleteImg } from '../controllers/upload.js';
 
 const router = express.Router();
 
 router.get('/', getCards);
 router.get('/user', auth, getUserCards);
 router.post('/', auth, upload.single('photo'), createCard);
-router.patch('/:id', auth, updateCard);
+router.patch('/:id', auth, deleteImg, upload.single('photo'), updateCard);
 router.patch('/:id/locked', auth, lockCard);
-router.delete('/:id', auth, deleteCard);
+router.delete('/:id', auth, deleteImg, deleteCard);
 
 export default router;
