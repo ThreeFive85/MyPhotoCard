81aae6bcc1ae1efb54d901556b65cc042cc20f04
박동혁 <kaje0god@naver.com>
2021-07-27 12:48:23 +0900

    Fix : res.status

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,14 +9,16 @@ const pool = mysql.createPool(db);
 
 export const signup = async(req, res) => { // async, await
 
-    const { name, email, password } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
 
     const connection = await pool.getConnection();
 
     try {
         const existingUser = await connection.query('select email from users where email = '+mysql.escape(req.body.email), {email:email} );
         // console.log(existingUser[0][0])
-        if(existingUser[0][0] !== undefined) return res.status(400).json({ message: "존재하는 유저입니다." });
+        if(existingUser[0][0]) return res.status(404).json({ message: "존재하는 유저입니다." });
+
+        if(password !== confirmPassword) return res.status(404).json({ message: "비밀번호가 일치하지 않습니다." });
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -47,12 +49,12 @@ export const signin = async(req, res) => { // async, await
 
     try {
         const existingUser = await connection.query('select * from users where email = '+mysql.escape(req.body.email), {email:email} );
-        // console.log(existingUser[0][0].password)
-        if(existingUser[0][0] === undefined) return res.status(404).json({ message: "유저를 찾을 수 없습니다" });
+        // console.log(existingUser[0][0])
+        if(!existingUser[0][0]) return res.status(404).json({ message: "유저를 찾을 수 없습니다" });
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser[0][0].password);
 
-        if(!isPasswordCorrect) return res.status(400).json({ message: "잘못된 암호입니다" });
+        if(!isPasswordCorrect) return res.status(404).json({ message: "잘못된 암호입니다" });
 
         const user = existingUser[0][0].email
         //console.log("user", user)
