a65c9f57141e2696962eac6c411a06eaa1c2cb2e
박동혁 <kaje0god@naver.com>
2021-07-31 18:34:27 +0900

    Fix: delete upload middleware to updateCard router

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', getCards);
 router.get('/user', auth, getUserCards);
 router.post('/', auth, upload.single('photo'), createCard);
-router.patch('/:id', auth, deleteImg, upload.single('photo'), updateCard);
+router.patch('/:id', auth, updateCard);
 router.patch('/:id/locked', auth, lockCard);
 router.delete('/:id', auth, deleteImg, deleteCard);
 
